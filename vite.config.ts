7c0f772d4a7d14df9bef927d6559e80c022bc5f6
MinhKha <minhkha@lutech.ltd>
2025-03-11 16:48:28 +0700

    feat:config prettier and eslint

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig, loadEnv, UserConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import tailwindcss from "@tailwindcss/vite";
+import path from "node:path";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-})
+export default defineConfig(({ mode }): UserConfig => {
+  const ENV = loadEnv(mode, process.cwd());
+  const port = ENV.VITE_PORT || 8080;
+  return {
+    plugins: [vue(), tailwindcss()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+    server: {
+      port: +port,
+    },
+    preview: {
+      port: +port,
+    },
+  };
+});
